5c9100842d8e0a95f874d695544f443dacd87021
Bhaskar-4444 <yuvaagaming2@gmail.com>
2025-08-09 21:09:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const faqItems = document.querySelectorAll('.faq-item h3');
-    faqItems.forEach(item => {
-        item.addEventListener('click', () => {
-            const answer = item.nextElementSibling;
-            const toggleIcon = item.querySelector('.toggle-icon');
-            answer.classList.toggle('active');
-            toggleIcon.textContent = answer.classList.contains('active') ? '−' : '+';
-        });
-    });
-});
+
